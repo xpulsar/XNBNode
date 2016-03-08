4a4ebfe88dec4412faae5134c87994e3b0feb895
Ian Ornelas <ian.orn@gmail.com>
2016-03-08 07:50:48 -0300

    Reformatting of map handling to be more inline with image handling

--- contenteditor.js
+++ contenteditor.js
@@ -4,6 +4,8 @@ let util = require('./util');
 let fs = require('fs');
 let PNG = require('pngjs').PNG;
 let path = require('path');
+let reader = require('./reader');
+let writer = require('./writer');
 
 
 function objectWalk(object, path, cb) {
@@ -25,26 +27,16 @@ function objectWalk(object, path, cb) {
 }
 
 function onPresave(outputFile, data) {
-    let map = processMap(data.content, true);
-
-    if (map != null) {
-        let ext = path.extname(outputFile);
-        let fileName = path.basename(outputFile, ext) + '.tbin';
-        let filePath = path.join(path.dirname(outputFile), fileName);
-        fs.writeFileSync(filePath, map);
-        
-        data.content.data.tBinName = fileName;
-    }
-
     let images = [];
+    let maps = [];
 
     objectWalk(data.content, (object, path) => {
         if(object.type == 'Texture2D') images.push(extractImage(object, path, outputFile));
+        else if(object.type == 'TBin') maps.push(extractMap(object, path, outputFile));
     });
 
-    if(images.length) {
-        data.extractedImages = images;
-    }
+    if(images.length) data.extractedImages = images;
+    if(maps.length) data.extractedMaps = maps;
 
     return data;
 }
@@ -52,18 +44,18 @@ function onPresave(outputFile, data) {
 exports.onPresave = onPresave;
 
 function onPostload(inputFile, data) {
-    if (util.isTypeObject(data.content) && data.content.type == 'TBin') {
-        let map = fs.readFileSync(path.join(path.dirname(inputFile), data.content.data.tBinName));
-        data.content.data.data = map;
-        let mapProcessed = processMap(data.content, false);
-        data.content.data.data = mapProcessed;
-    }
-
     let images = data.extractedImages || [];
-
     for(let i = 0; i < images.length; i++) {
         loadImage(images[i], inputFile, data);
     }
+
+    let maps = data.extractedMaps || [];
+    for(let i = 0; i < maps.length; i++) {
+        loadMap(maps[i], inputFile, data);
+    }
+
+    delete data.extractedImages;
+    delete data.extractedMaps;
 
     return data;
 }
@@ -77,98 +69,10 @@ function traversePath(object, path) {
     return traversePath(object[parts[0]], parts.slice(1).join('.'));
 }
 
-function getImageName(baseFile, contentPath) {
+function getPathBasename(baseFile, contentPath) {
     let ext = path.extname(baseFile);
-    return path.join(path.dirname(baseFile), path.basename(baseFile, ext) + '.' + contentPath + '.png');
-}
-
-function processMap(object, addPng) {
-    let data = object.data.data;
-
-    if (data != null && data.toString('utf8', 0, 6) === 'tBIN10') {
-        delete object.data.data;
-        let reader = require('./reader');
-        let writer = require('./writer');
-
-        let buffer = new reader.BufferConsumer(data);
-        let header = buffer.consume(6);
-        let out = new writer.BufferWriter();
-        out.concat(header);
-
-        let skipString = function() {
-            let skipBytes = buffer.consume(4).readInt32LE();
-            out.writeInt32LE(skipBytes);
-            out.concat(buffer.consume(skipBytes));
-        }
-
-        let skipProperties = function() {
-            let numberProperties = buffer.consume(4).readInt32LE();
-            out.writeInt32LE(numberProperties);
-
-            while (numberProperties-- > 0) {
-                // skip key string
-                skipString();
-
-                let propertyType = buffer.consume(1);
-                out.concat(propertyType);
-                switch (propertyType[0]) {
-                    case 0: // bool
-                        out.concat(buffer.consume(1));
-                        break;
-                    case 1: // int32
-                        out.concat(buffer.consume(4));
-                        break;
-                    case 2: // float (single)
-                        out.concat(buffer.consume(4));
-                        break;
-                    case 3: // string
-                        skipString();
-                        break;
-                }
-            }
-        }
-
-        //skip map id string
-        skipString();
-
-        //skip map description string
-        skipString();
-
-        skipProperties();
-
-        let numberTileSheets = buffer.consume(4).readInt32LE();
-        out.writeInt32LE(numberTileSheets);
-        let tileSheets = [];
-        while (numberTileSheets-- > 0) {
-            skipString(); // skip id
-            skipString(); // skip description
-
-            let strBytes = buffer.consume(4).readInt32LE();
-            let imgSource = buffer.consume(strBytes);
-
-            if (addPng) {
-                imgSource = imgSource + '.png';
-                tileSheets.push(imgSource);
-            } else {
-                tileSheets.push(imgSource);
-                imgSource = path.basename(imgSource, '.png');
-            }
-            
-            out.writeInt32LE(imgSource.length);
-            out.writeAscii(imgSource);
-
-            out.concat(buffer.consume(4 * 2 * 4)); // skip sizes, margin and spacing (4 "size" of 2 int32 each)
-
-            skipProperties();
-        }
-
-        out.concat(buffer.buffer); // done with tilesets, everything else doesn't matter
-
-        object.data.tileSheets = tileSheets;
-
-        return out.buffer;
-    }
-    return null;
+    let pathSuffix = contentPath ? '.' + contentPath : '';
+    return path.join(path.dirname(baseFile), path.basename(baseFile, ext) + pathSuffix);
 }
 
 function extractImage(object, path, outputFile) {
@@ -180,7 +84,7 @@ function extractImage(object, path, outputFile) {
 
     png.data = object.data.data;
 
-    let filename = getImageName(outputFile, path);
+    let filename = getPathBasename(outputFile, path) + '.png';
     let buffer = PNG.sync.write(png);
     fs.writeFileSync(filename, buffer);
 
@@ -194,7 +98,7 @@ function extractImage(object, path, outputFile) {
 }
 
 function loadImage(image, inputFile, data) {
-    let filename = getImageName(inputFile, image.path);
+    let filename = getPathBasename(inputFile, image.path) + '.png';
     let pngBuffer = fs.readFileSync(filename);
     let png = PNG.sync.read(pngBuffer);
 
@@ -205,3 +109,105 @@ function loadImage(image, inputFile, data) {
     container.height = png.height;
 }
 
+function processMap(data, tilesheetCallback) {
+    let buffer = new reader.BufferConsumer(data);
+    let header = buffer.consume(6);
+    let out = new writer.BufferWriter();
+    out.concat(header);
+
+    let skipString = function() {
+        let skipBytes = buffer.consume(4).readInt32LE();
+        out.writeInt32LE(skipBytes);
+        out.concat(buffer.consume(skipBytes));
+    };
+
+    let skipProperties = function() {
+        let numberProperties = buffer.consume(4).readInt32LE();
+        out.writeInt32LE(numberProperties);
+
+        while (numberProperties-- > 0) {
+            // skip key string
+            skipString();
+
+            let propertyType = buffer.consume(1);
+            out.concat(propertyType);
+            switch (propertyType[0]) {
+                case 0: // bool
+                    out.concat(buffer.consume(1));
+                    break;
+                case 1: // int32
+                    out.concat(buffer.consume(4));
+                    break;
+                case 2: // float (single)
+                    out.concat(buffer.consume(4));
+                    break;
+                case 3: // string
+                    skipString();
+                    break;
+            }
+        }
+    };
+
+    //skip map id string
+    skipString();
+
+    //skip map description string
+    skipString();
+
+    skipProperties();
+
+    let numberTileSheets = buffer.consume(4).readInt32LE();
+    out.writeInt32LE(numberTileSheets);
+    let tileSheets = [];
+    while (numberTileSheets-- > 0) {
+        skipString(); // skip id
+        skipString(); // skip description
+
+        let strBytes = buffer.consume(4).readInt32LE();
+        let imgSource = buffer.consume(strBytes);
+
+        imgSource = tilesheetCallback(imgSource);
+
+        out.writeInt32LE(imgSource.length);
+        out.writeAscii(imgSource);
+
+        out.concat(buffer.consume(4 * 2 * 4)); // skip sizes, margin and spacing (4 "size" of 2 int32 each)
+
+        skipProperties();
+    }
+
+    out.concat(buffer.buffer); // done with tilesets, everything else doesn't matter
+    return out.buffer;
+}
+
+function extractMap(object, path, outputFile) {
+    let tilesheets = [];
+
+    let mapBuffer = processMap(object.data.data, tilesheet => {
+        tilesheet += '.png';
+        tilesheets.push(tilesheet);
+        return tilesheet;
+    });
+
+    let mapFile = getPathBasename(outputFile, path) + '.tbin';
+    fs.writeFileSync(mapFile, mapBuffer);
+
+    delete object.data.data;
+
+    return {
+        path: path,
+        tilesheets: tilesheets
+    };
+}
+
+function loadMap(map, inputFile, data) {
+    let mapFile = getPathBasename(inputFile, map.path) + '.tbin';
+    let mapBuffer = fs.readFileSync(mapFile);
+
+    mapBuffer = processMap(mapBuffer, tilesheet => {
+        return path.basename(tilesheet, '.png');
+    });
+
+    let container = traversePath(data.content, map.path);
+    container.data = mapBuffer;
+}
